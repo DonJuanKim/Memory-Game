341f597d353b5dd5c58182200ff88648b91d0ea9
Donghwan Kim <donghwan.kim92@gmail.com>
2021-08-12 19:20:04 +0200

    sixth

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const card = document.getElementById("card");
+var cards = document.querySelectorAll('div.card');
 
-card.addEventListener("click", flipCard);
+for(let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function(){
+    cards[i].classList.toggle('flipCard');
+    });
+};
 
-function flipCard() {
-    card.classList.toggle("flipCard");
-}
+// function selectCard(e) {
+//         e = e || window.event;
+//         e = e.target || e.srcElement;
+//         console.log("className: " + e.classList);
+//     }
