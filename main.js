b115fca59a38dbf68280841affd09b120986a66a
Donghwan Kim <donghwan.kim92@gmail.com>
2021-08-18 16:00:58 +0200

    seventh

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-var cards = document.querySelectorAll('div.card');
+const cards = document.querySelectorAll('div.card');
+const startBtn = document.getElementById('startBtn');
+
+
+// Flip cards
 
 for(let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', function(){
-    cards[i].classList.toggle('flipCard');
-    });
-};
+    cards[i].classList.add('flippedCard');
+    const flippedCard = document.querySelectorAll('.flippedCard');
+    
+    if (flippedCard.length == 2) {
+        console.log("it's two");
+        checkMatch(flippedCard);
+    }  
+    })};
 
-// function selectCard(e) {
-//         e = e || window.event;
-//         e = e.target || e.srcElement;
-//         console.log("className: " + e.classList);
-//     }
+function checkMatch(flippedCard) {
+    console.log(flippedCard);
+    let pickedCards = document.getElementsByTagName('img');
+    let firstPick = pickedCards[0].id;
+    let secondPick = pickedCards[1].id;
+    console.log(firstPick);
+    console.log(secondPick);
+
+    if (firstPick == secondPick) {
+      console.log("match");
+      
+    } else {
+        flippedCard.classList.remove("flippedCard");
+        console.log("try again");
+    }
+}
+
+// shuffle cards 
+
+startBtn.addEventListener('click', function(){
+    shuffleCards();
+});
+
+function shuffleCards() {
+for(let i = cards.length - 1; i > 0; i--){
+    let randIndex = Math.floor(Math.random() * (i+1));
+    cards[randIndex].style.order = i;
+    cards[i].style.order = randIndex;
+};
+}
